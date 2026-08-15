58eecca33aec2011518cca37c97544d8e5578146
Abhinav Dhandu <abhinavdhandhu13@gmail.com>
2026-08-15 12:12:04 +0530

    Improve readability and add comments in #!190C.py
    Refactor code for clarity and add comments explaining logic.

--- #!190C.py
+++ #!190C.py
@@ -2,25 +2,25 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-
-    ones = 0
+    # to count teh numbers in the card
+    ones = 0 
     others = 0
-    cap = 0
+    cap = 0 # extra members
 
     for x in a:
         if x == 1:
             ones += 1
         else:
             others += x
-            if x > 3:
-                cap += (x - 2) // 2
+            if x > 3: 
+                cap += (x - 2) // 2 # rounding off 
 
-    if ones + others < 3:
+    if ones + others < 3: # simply imposible
         print(0)
-    elif cap >= ones:
+    elif cap >= ones: # just enough to fulfill condition
         print(ones + others)
-    else:
-        if ones == n - 1:
+    else: # not enough
+        if ones == n - 1: # if all are one tehn there is extra space
             print(others + cap + 1)
         else:
             print(others + cap)
