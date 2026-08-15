0dc2122e31397e2c476b38cc23f0d55f0a5aaba6
Abhinav Dhandu <abhinavdhandhu13@gmail.com>
2026-08-15 12:41:09 +0530

    Update 122A_lucky_div.py

--- 122A_lucky_div.py
+++ 122A_lucky_div.py
@@ -1,11 +1,11 @@
 n = int(input())
-k=[4 , 7, 44, 47, 74, 77, 444, 447, 474, 477, 777, 744, 747, 774]
+k=[4 , 7, 44, 47, 74, 77, 444, 447, 474, 477, 777, 744, 747, 774] # all possible combinations
 l = list(str(n))
-if n %4 == 0 or n % 7 == 0:
+if n %4 == 0 or n % 7 == 0: # base condition
     print("YES")
 else:
     for i in k:
-        if n % i == 0:
+        if n % i == 0: # final condition
             print("YES")
             exit()
     print("NO")
