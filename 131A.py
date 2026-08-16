c3fb66dd4a5bcce0fd7028361743ca832b5b35e2
Abhinav Dhandu <abhinavdhandhu13@gmail.com>
2026-08-16 13:06:59 +0530

    Update 131A.py

--- 131A.py
+++ 131A.py
@@ -1,10 +1,10 @@
 s = input()
 c = 0
 for i in s:
-    if i.isupper():
+    if i.isupper(): # checl how many are upper
         c+= 1
-if c == len(s) - 1 and s[0].islower():
-    s = s[0].upper() + s[1:].lower()
-elif c == len(s):
+if c == len(s) - 1 and s[0].islower(): # if first letter is lower and rest are upper
+    s = s[0].upper() + s[1:].lower() 
+elif c == len(s): # if all are capital
     s = s.lower()
 print(s)
